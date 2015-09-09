0aedc7cfe76a2bf379b4e3cd4a69b7956c919399
Firestack <me@stackfire.io>
2015-09-09 15:46:35 -0600

    Commiting For the day lots of changes

---
+++ CXX/Hailstone/include/utils.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <utility>
+#include <math.h>
+#include <stdint.h>
+#include <cstdint>
+
+std::pair<bool, unsigned int> chkPow2(uint64_t N)
+{
+	if (N % 2 != 0) { return std::make_pair<bool, unsigned int>(false, 0); }
+
+	double t = log2(N);
+
+	//std::cout << t << "\t" << fmod(t, 1.0) << "\t" << (fmod(t, 1.0) == 0) << std::endl;
+
+	return std::make_pair<bool, unsigned int>(fmod(t, 1.0) == 0, fmod(t, 1.0));
+}
+
+
+uint64_t myPow(uint8_t A, uint8_t B)
+{
+	uint64_t Total = 1;
+	for (int index = 1; index <= B; index++) { Total *= A; }
+
+	return Total;
+}
--- CXX/Hailstone/src/hailstone.cxx
+++ CXX/Hailstone/src/hailstone.cxx
@@ -1,46 +1,63 @@
+#pragma once
 #include <vector>
 #include <utility>
 #include <iostream>
 #include <fstream>
 #include <assert.h>
 #include <thread>
+#include <math.h>
+#include <omp.h>
+#include <fstream>
+#include "../include/utils.h"
 #include <Windows.h>
-#include <omp.h> 
+#include <algorithm>
 
+#include <stdio.h>  /* defines FILENAME_MAX */
+#ifdef WIN32
+	#include <Windows.h>
+	#include <direct.h>
+	#define GetCurrentDir _getcwd
+#else
+	#include <unistd.h>
+	#define GetCurrentDir getcwd
+#endif
 
+typedef uint64_t ul_int;
 
-std::vector<unsigned int> hailstone(unsigned int n)
+std::vector<ul_int> & hailstone(ul_int n)
 {
 	// Create values vector
-	std::vector<unsigned int> v;
+	auto v = new std::vector<ul_int>() ;
 
 	// Super awesome forloop to calc the sequence, TODO: time against while loop
-	for (int i = n; i > 1; i = (i % 2 == 0 ? i / 2 : 3 * i + 1)) { v.push_back(i); }
+	for (ul_int i = n; i > 1; i = (i % 2 == 0 ? i / 2 : 3 * i + 1)) { v->push_back(i); }
 
 	// Add final value to vector since for exits on `1`
-	v.push_back(1);
+	v->push_back(1);
 	
 	// Annnnnnnd return
-	return v;
+	return *v;
 }
 
-unsigned int hailstoneLength(unsigned int n)
-{// Same function as hailstone except counts just length instead of the memory useage of vector
+
+ul_int hailstoneLength(ul_int n)
+{// Same function as hailstone except counts just length instead of the memory usage of vector
 	
 	// Allocate for forloop exit on `1`
-	unsigned int tLength = 1;
+	ul_int tLength = 1;
 	// Super awesome forloop
-	for (int i = n; i > 1; i = (i % 2 == 0 ? i / 2 : 3 * i + 1)) { tLength++; }
+	for (ul_int i = n; i > 1; i = (i % 2 == 0 ? i / 2 : 3 * i + 1)) { tLength++; }
 	// Annnnnnnd return length
 	return tLength;
 }
 
-std::pair<unsigned int, unsigned int> maxHailstoneLength(unsigned int start = 1, unsigned int end = 2, uint32_t step = 1)
+
+std::pair<ul_int, ul_int> maxHailstoneLength(ul_int start = 1, ul_int end = 2, ul_int step = 1)
 {
 	// Create tracking variables, cVal being signed long long was required by openMP
 	long long cVal = 0;
-	uint32_t largest = 0;
-	uint32_t largestNVal = start;
+	ul_int largest = 0;
+	ul_int largestNVal = start;
 
 	// Print general information
 	std::cout << "doing numbers between : " << start << " and " << end << " for a total of: " << end - start << std::endl;
@@ -62,7 +79,7 @@ std::pair<unsigned int, unsigned int> maxHailstoneLength(unsigned int start = 1,
 		for (cVal = start; cVal <= end; cVal += step)
 		{
 			// Calc current 'n' value for hailstone
-			uint32_t cLength = hailstoneLength(cVal);
+			ul_int cLength = hailstoneLength(cVal);
 			// Test largest and set values
 			if (cLength > largest)
 			{
@@ -80,13 +97,105 @@ std::pair<unsigned int, unsigned int> maxHailstoneLength(unsigned int start = 1,
 	return std::pair<uint32_t, uint32_t>(largest, largestNVal);
 }
 
-int WinMain()
+
+std::vector<std::vector<ul_int>> hailstones_mutliprocess(ul_int start = 1, ul_int end = 1000, ul_int step = 1)
 {
-	 
-	// Return largest size and print
-	auto largestSize = maxHailstoneLength(1, 100'000'000);
-	std::cout << "Length: "<< largestSize.first << "\t NValue: " << largestSize.second << std::endl;
+	// Store values
+	std::vector<std::vector<ul_int>> totals;
+
+	// Print general information
+	std::cout << "doing numbers between : " << start << " and " << end << " for a total of: " << (end - start)/step << std::endl;
+
+	// Get execution start time
+	auto execStart = std::chrono::high_resolution_clock::now();
+
+	// OpenMP pragma to create parallel for loop
+	#pragma omp parallel
+	{
 	
+	#pragma omp master
+	{	std::cout << "Using '" << omp_get_num_threads() << "' thread(s)" << std::endl;	}
+
+	std::vector<std::vector<ul_int>> vec_private;
+	#pragma omp for nowait
+		for (long long int index = start; index <= end; index += step)
+		{
+			vec_private.push_back(hailstone(index));
+		}
+	#pragma omp critical
+		{ totals.insert(totals.end(), vec_private.begin(), vec_private.end()); }
+	}
+
+	// End Chrono timeit of execution and print
+	auto total = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - execStart).count();
+	std::cout << "total time: " << total << "ms" << std::endl;
+
+	return totals;
+}
+
+void write_hailstones(std::string filename, std::vector<std::vector<ul_int>> &data)
+{
+	auto sortlambda = [](std::vector<ul_int> a, std::vector<ul_int> b) {return (a[0] < b[0]); };
+	std::sort(data.begin(), data.end(), sortlambda);
+
+	// Print current directory
+	char cCurrentPath[FILENAME_MAX];
+
+	if (!GetCurrentDir(cCurrentPath, sizeof(cCurrentPath)))
+	{
+		throw errno;
+	}
+
+	cCurrentPath[sizeof(cCurrentPath) - 1] = '\0'; /* not really required */
+
+	std::cout << "The current working directory is " << cCurrentPath << std::endl
+		<< "writing file..." << std::endl;
+
+	std::ofstream fout(filename);
+	
+	fout << "Numbers" << ", " << "Length" << ", " << "Sequence" << std::endl;
+	for (auto line : data)
+	{
+		fout << line[0] << ", " << line.size() << ", ";
+		for (auto value : line)
+		{
+			fout << ", " << value;
+		}
+		fout << std::endl;
+	}
+
+	fout.close();
+
+	std::cout << "Finished." << std::endl;
+}
+
+int main()
+{
+
+	std::locale loc("");
+	std::cout.imbue(loc);
+
+	uint64_t expo = 0;
+
+	//std::cout << "Enter order of magnitude(int): ";
+	//std::cin >> expo;
+
+	//expo = myPow(10, expo);
+
+	//std::cout << "Correct? :" << expo;
+
+	//std::cin.ignore();
+	//std::cin.get();
+	
+	// Return largest size and print
+	
+	std::cout << "Enter top value... ";
+	std::cin >> expo;
+
+	const ul_int Value = expo;
+	//auto largestSize = maxHailstoneLength(1, Value);
+	//std::cout << "Length: "<< largestSize.first << "\t NValue: " << largestSize.second << std::endl;
+	write_hailstones("output.csv", hailstones_mutliprocess(1, Value));
 	// Wait for user input to close program
 	std::cin.get();
 	return 0;
